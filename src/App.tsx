89dc3729e4b23b4c8975b6c1c92146119278008c
William Tomé <william.tomenh@gmail.com>
2020-10-12 19:53:22 -0300

    modificado layout da tela principal e criada a tela de localização de orfanatos.

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
+
 import './styles/global.css';
-import './styles/landing.css';
-import logoImg from './images/logo.svg';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Leve felicidade para o mundo.</h1>
-          <p>
-            Visite orfanatos e mude o dia de muitas crianças.
-          </p>
-        </main>
-
-        <div className="location">
-          <strong>Canoas</strong>
-          <span>Rio Grande do Sul</span>
-        </div>
-
-        <a href="" className="enter-app">
-          >
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
